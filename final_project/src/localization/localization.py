cc131ec21295160893670b03df28d79b583e0a62
Ilya Ryakin <ryakin.is@phystech.edu>
2021-01-25 23:53:25 +0300

    add loc files

---
+++ final_project/src/localization/localization.py
@@ -0,0 +1,15 @@
+
+
+class Localization:
+    def __init__(self):
+        pass
+
+    
+    def update(self, data):
+        pass 
+
+    def return_robot_position(self):
+        pass
+
+    def return_ball_position(self):
+        pass
