b2bab4cb4e7182df3b64f3c0a6cf416a3724633c
Merrah <ienjoyeatingcorpses@gmail.com>
2021-05-11 21:54:49 +0300

    model fixes

--- final_project/src/model/model.py
+++ final_project/src/model/model.py
@@ -41,13 +41,13 @@ class Model:
         b = self.camera_tilt
 
         r2cam_rot_matrix = [[math.cos(a) * math.cos(b),
-                             math.sin(a),
-                             -math.cos(a) * math.sin(b)],
-                            [-math.sin(a) * math.cos(b),
+                             math.sin(a) * math.cos(b),
+                             math.sin(b)],
+                            [-math.sin(a),
                              math.cos(a),
-                             math.sin(a) * math.sin(b)],
-                            [math.sin(b),
-                             0.0,
+                             0.0],
+                            [-math.sin(b) * math.cos(a),
+                             -math.sin(a) * math.sin(b),
                              math.cos(b)]]
 
         return self.matrix_dot_vec(r2cam_rot_matrix, r_coords)
@@ -59,44 +59,18 @@ class Model:
 
         cam2r_rot_matrix = [[math.cos(a) * math.cos(b),
                              -math.sin(a),
-                             math.cos(a) * math.sin(b)],
+                             -math.cos(a) * math.sin(b)],
                             [math.sin(a) * math.cos(b),
                              math.cos(a),
-                             math.sin(a) * math.sin(b)],
-                            [-math.sin(b),
+                             -math.sin(a) * math.sin(b)],
+                            [math.sin(b),
                              0.0,
                              math.cos(b)]]
 
         return self.matrix_dot_vec(cam2r_rot_matrix, cam_coords)
 
-    # transformation from robot coords to pixel picture coords
-    def r2pic(self, xb, yb):
-        # xb, yb - coords of the ball in the robot system
 
-        # transformation to camera coords and back to apply head-to-base matrix
-        cam_coords = self.r2cam(xb, yb)
-        fixed_cam_coords = self.matrix_dot_vec(self.head_to_base_matrix,
-                                               cam_coords)
-        xb, yb, zb = self.cam2r(fixed_cam_coords)
-
-        # rotating the coordinate system by the cameraPan
-        # xbi,ybi - coords of the ball in the same system after rotation
-        xbi = xb * math.cos(self.camera_pan) + yb * math.sin(self.camera_pan)
-        ybi = yb * math.cos(self.camera_pan) - xb * math.sin(self.camera_pan)
-
-        # taking into account the case when the ball is behind the camera
-        if xbi <= 0:
-            print("Ball behind the camera")
-            return (-1, -1)
-
-        # alp - artificially created angle, used for simplification
-        # x,y - coords of the ball in system, parallel to the camera screen
-        # u,v - pixel coords of the ball in the screen system
-        alp = math.atan(self.h / xbi) + self.camera_tilt
-        x = ybi / math.cos(alp) / math.sqrt(xbi**2 + self.h ** 2)
-        y = math.tan(alp)
-
-        # applying the lense distortion-fix formula
+    def distortion_straight(self, x, y):
         r_sq = x ** 2 + y ** 2
         coef_numerator = (1 +
                           self.k_coefs[0] * r_sq +
@@ -108,54 +82,121 @@ class Model:
                             self.k_coefs[4] * (r_sq ** 2) +
                             self.k_coefs[5] * (r_sq ** 3))
 
-        coef = coef_numerator / coef_denominator
+        g = coef_numerator / coef_denominator
 
         # x_cor, y_cor - ball cords in artificially created coord system
-        x_cor_parts = [x * coef,
+        x_cor_parts = [x * g,
                        2 * self.p_coefs[0] * x * y,
                        self.p_coefs[1] * (r_sq + 2 * x ** 2)]
         x_cor = sum(x_cor_parts)
 
-        y_cor_parts = [y * coef,
+        y_cor_parts = [y * g,
                        2 * self.p_coefs[1] * x * y,
                        self.p_coefs[0] * (r_sq + 2 * y ** 2)]
         y_cor = sum(y_cor_parts)
 
+        return [x_cor, y_cor]
+
+    def dist_error(self, x, y, c1, c2):
+        r1, r2 = self.distortion_straight(x, y)
+        return [r1 - c1, r2 - c2]
+
+    def dist_er_jac_inv(self, x, y):
+        r_sq = x ** 2 + y ** 2
+        coef_numerator = (1 +
+                          self.k_coefs[0] * r_sq +
+                          self.k_coefs[1] * (r_sq ** 2) +
+                          self.k_coefs[2] * (r_sq ** 3))
+
+        coef_denominator = (1 +
+                            self.k_coefs[3] * r_sq +
+                            self.k_coefs[4] * (r_sq ** 2) +
+                            self.k_coefs[5] * (r_sq ** 3))
+
+        g = coef_numerator / coef_denominator
+        gdx = ((2 * self.k_coefs[0] * x + 
+            4 * self.k_coefs[1] * x * r_sq +
+            6 * self.k_coefs[2] * x * r_sq**2) / 
+            coef_denominator - 
+            (coef_numerator * 
+            (2 * self.k_coefs[3] * x + 
+                4 * self.k_coefs[4] * x * r_sq +
+                6 * self.k_coefs[5] * x * r_sq**2)) /
+            coef_denominator**2)
+
+        gdy = ((2 * self.k_coefs[0] * y + 
+            4 * self.k_coefs[1] * y * r_sq +
+            6 * self.k_coefs[2] * y * r_sq**2) / 
+            coef_denominator - 
+            (coef_numerator * 
+            (2 * self.k_coefs[3] * y + 
+                4 * self.k_coefs[4] * y * r_sq +
+                6 * self.k_coefs[5] * y * r_sq**2)) /
+            coef_denominator**2)
+
+        a11 = g + x * gdx + 2 * self.p_coefs[0] * y + 6 * self.p_coefs[1] * x
+        a12 = y * gdx + 2 * self.p_coefs[0] * x + 2 * self.p_coefs[1] * y
+        a21 = x * gdy + 2 * self.p_coefs[0] * x + 2 * self.p_coefs[1] * y
+        a22 = g + y * gdy + 2 * self.p_coefs[1] * x + 6 * self.p_coefs[0] * y
+
+        det = a11 * a22 - a12 * a21
+        return [[a22 / det, -a12 / det], [-a21 / det, a11 / det]]
+
+
+    def distortion_inverse(self, c1, c2):
+        x = c1
+        y = c2
+        for i in range(100):
+            x_new = (x - 
+                    (self.dist_er_jac_inv(x, y)[0][0] * self.dist_error(x, y, c1, c2)[0] +
+                     self.dist_er_jac_inv(x, y)[0][1] * self.dist_error(x, y, c1, c2)[1]))
+            y_new = (y -
+                    (self.dist_er_jac_inv(x, y)[1][0] * self.dist_error(x, y, c1, c2)[0] +
+                     self.dist_er_jac_inv(x, y)[1][1] * self.dist_error(x, y, c1, c2)[1]))
+
+            if abs(x_new - x) + abs(y_new - y) < 0.00001:
+                break
+            else:
+                x = x_new
+                y = y_new
+        return [x, y]
+
+
+    def r2pic(self, xb, yb):
+        # xb, yb - coords of the ball in the robot system
+
+        # transformation to camera coords and back to apply head-to-base matrix
+        cam_coords = self.r2cam(xb, yb)
+        x_c, y_c, z_c = self.matrix_dot_vec(self.head_to_base_matrix,
+                                               cam_coords)
+        x = y_c / x_c
+        y = z_c / x_c
+
+        # applying the lense distortion-fix formula
+        x_cor, y_cor = self.distortion_straight(x, y)
+
         # u, v - pixel coords of the ball
         u = -x_cor * self.cam_matrix[0][0] + self.cam_matrix[0][2]
-        v = y_cor * self.cam_matrix[1][1] + self.cam_matrix[1][2]
+        v = -y_cor * self.cam_matrix[1][1] + self.cam_matrix[1][2]
         return (int(u), int(v))
 
-    # transformation from pixel coords to robot coords
+
     def pic2r(self, u, v):
         # u,v - pixel coords of the ball in the screen system
 
         # x,y - coords of the ball in the coordinate system,
         # parallel to the camera screen
-        x = (float(u) - self.cam_matrix[0][2]) / self.cam_matrix[0][0]
-        y = (float(v) - self.cam_matrix[1][2]) / self.cam_matrix[1][1]
+        x = -(float(u) - self.cam_matrix[0][2]) / self.cam_matrix[0][0]
+        y = -(float(v) - self.cam_matrix[1][2]) / self.cam_matrix[1][1]
 
-        # alp,bet - vertical and horizontal angles
-        # of the ball radius-vector relative to the camera 0,0 direction
-        alp = math.atan(y)
-        bet = math.atan(x)
+        # transformation of x and y using the distortion coefficients
+        x, y = self.distortion_inverse(x, y)
 
-        robot_alp = alp - self.camera_tilt
+        x_cam = -self.h / (math.sin(self.camera_tilt) + y * math.cos(self.camera_tilt))
+        x_cam = abs(x_cam)
+        y_cam = x_cam * x
+        z_cam = x_cam * y
 
-        # xb, yb - coordinates of the ball in the system turned by cameraPan
-        xb = self.h / math.tan(robot_alp)
-        yb = math.tan(bet) * math.sqrt(self.h ** 2 + xb ** 2)
+        x_r, y_r, _ = self.cam2r([x_cam, y_cam, z_cam])
 
-        # xb_r, yb_r - coordinates of the ball in the robot system
-        xb_r = xb * math.cos(self.camera_pan) + yb * math.sin(self.camera_pan)
-        yb_r = yb * math.cos(self.camera_pan) - xb * math.sin(self.camera_pan)
-
-        # transformation to camera coords and back to apply head-to-base matrix
-        cam_coords = self.r2cam(xb_r, -yb_r)
-        fixed_cam_coords = self.matrix_dot_vec(self.head_to_base_matrix,
-                                               cam_coords)
-
-        # final coords in robot system
-        xb_r, yb_r, zb_r = self.cam2r(fixed_cam_coords)
-
-        return (xb_r, yb_r)
+        return (x_r, y_r)
